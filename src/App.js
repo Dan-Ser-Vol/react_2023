6e95dc447f567546b653df4075c9114ee6451c66
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-04-12 20:41:53 +0300

    some commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,8 @@ import RickAndMorty from './components/RickAndMorty/RickAndMorty';
 function App() {
     return (
         <div className="App">
-            <h1>_____________________________________Simpsons______________________________________</h1>
             <Simpsons/>
-            <h2>_________________________________Rick And Morty________________________________</h2>
             <RickAndMorty/>
-            <h2>__________________________________The End___________________________</h2>
         </div>
     );
 }
