a2f87b5de20886b33dd439ebb088b3954a5e1036
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-04-13 18:07:32 +0300

    finish homework

--- src/components/Users/Posts.js
+++ src/components/Users/Posts.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import style from './Users.module.css'
 
 const Posts = ({post}) => {
     return (
-        <div>
+        <div className={style.item}>
             <div> user id: {post.userId}</div>
             <h3>TITLE: {post.title}</h3>
             <div>BODY: {post.body}</div>
